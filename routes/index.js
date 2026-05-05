8fda61250eb2477c4bb2d3ce0dac9929c3d39572
Jordi Divison i Carreras <21755948h@iespoblenou.org>
2026-05-05 16:35:28 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const iaRoutes = require('./ia');
-const scheduleRoutes = require('./schedule');
+const iaRoutes = require("./ia");
+const scheduleRoutes = require("./schedule");
 
-router.use('/ia', iaRoutes);
-router.use('/schedule', scheduleRoutes);
+router.use("/ia", iaRoutes);
+router.use("/schedule", scheduleRoutes);
+router.use("/auth", require("./auth"));
 
 // Pendiente añadir las rutas CRUD
 // router.use('/departments', require('./departments'));
